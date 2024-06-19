446a9d6fb35ba10905d766e3da6146be4543dc4f
blitz2002jp <blitz2002jp@gmail.com>
2024-06-19 14:27:22 +0900

    Initial Commit

---
+++ AutoUnitTest2/AutoUnitTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  AutoUnitTest2App.swift
+//  AutoUnitTest2
+//
+//  Created by masazumi oeda on 2024/06/19.
+//
+
+import SwiftUI
+
+@main
+struct AutoUnitTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
